6057f937d49c1e465a2823efbb1da543005878f0
sniper <sdqdtxl@gmail.com>
2020-04-21 17:57:12 +0800

    20200421 DP

--- src/main/java/com/excercise/LeetCode/S322.java
+++ src/main/java/com/excercise/LeetCode/S322.java
@@ -13,23 +13,58 @@ public class S322 {
     public S322() {
     }
 
-    public int coinChange(int[] coins, int amount){
-//        int[] dp = new int[amount];
-//        dp[0] = 2<<31-1;
-        if(coins.length == 0){
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for(int coin : coins){
+            dp[coin] = 1;
+        }
+        for(int i=0;i<=amount;i++){
+            for(int coin : coins){
+                if(i+coin<=amount) {
+                    dp[i + coin] = Math.min(dp[i + coin], dp[i] + 1);
+                }
+            }
+        }
+        return (dp[amount]==Integer.MAX_VALUE) ? dp[amount] : -1;
+    }
+    /**
+     * Description: 暴力递归
+     * @Param: [coins, amount, count]
+     * @return: void
+     * @Author: sniper
+     * @Date: 2020/4/21 17:34
+
+    int min = Integer.MAX_VALUE;
+
+    public void findMinCnt(int[] coins, int amount, int count) {
+
+        if (amount < 0) {
+            return;
+        }
+        if (amount == 0) {
+            min = Math.min(min, count);
+        }
+
+        for (int i = 0;i < coins.length;i++) {
+            findMinCnt(coins, amount - coins[i], count+1);
+        }
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        if (coins.length == 0) {
             return -1;
         }
-        int min = Integer.MAX_VALUE;
-        for(int coin : coins){
-           if(coin == amount){
-               return 1;
-           }if(coin > amount){
-               return Integer.MAX_VALUE;
-           }
+
+        findMinCnt(coins, amount, 0);
+
+        if (min == Integer.MAX_VALUE) {
+            return -1;
         }
-        for(int coin : coins){
-            min = Math.min(min, 1+coinChange(coins, amount-coin));
-        }
-        return min>Integer.MAX_VALUE?-1:min;
+        return min;
     }
+     */
 }
